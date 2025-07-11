2bd0a7b54edd91428dd1bf63a472281d89efe954
Seyi <oluwaseyisalisu1@gmail.com>
2025-07-11 02:10:36 -0400

    object for outputs

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Welcome to the Mixed Messages game!");
+const phrases = {
+    signs:['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    luck:['good luck', 'bad luck', 'ok luck'],
+    news:{
+        goodNews:['You will find a new opportunity today.', 'A friend will bring you good news.', 'You will receive a compliment that makes your day.'],
+        badNews:['You might face a minor setback today.', 'A misunderstanding could lead to some frustration.', 'You may lose something important.'],
+        okNews:['You will have a normal day with no surprises.', 'You might hear some neutral news from a colleague.', 'A routine task will go smoothly.']
+    }
+}
